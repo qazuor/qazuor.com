4016880141a1c8011fbd1e549d16a04c8f60d328
qazuor <qazuor@gmail.com>
2025-11-11 20:10:39 -0300

    style: enhance wave divider and update global styles

--- src/components/ui/dividers/index.ts
+++ src/components/ui/dividers/index.ts
@@ -1,4 +1,3 @@
 export { default as DiagonalDivider } from './DiagonalDivider.astro';
-export { default as ParticlesDivider } from './ParticlesDivider.astro';
 export { default as WaveDivider } from './WaveDivider.astro';
 export { default as ZigzagDivider } from './ZigZagDivider.astro';

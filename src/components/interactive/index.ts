4efb241c91f9ab50bc8cca4e2946c6db7d78ff41
qazuor <qazuor@gmail.com>
2025-11-14 15:40:52 -0300

    refactor: convert SkillsGrid from TSX to Astro
    - Replace React-based SkillsGrid.tsx with Astro component
    - Update SkillsSection to use new Astro component
    - Remove client-side rendering for better performance
    - Update component exports and imports

--- src/components/interactive/index.ts
+++ src/components/interactive/index.ts
@@ -6,7 +6,6 @@ export * from './CommandPalette';
 export * from './LanguageSelector';
 export * from './ScrollAnimatedSection';
 export * from './ScrollToTop';
-export * from './SkillsGrid';
 export * from './ThemeToggle';
 export { default as Timeline } from './Timeline';
 export { default as TimelineDesktop } from './TimelineDesktop';
